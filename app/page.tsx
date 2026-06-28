35f3eea49db2c76195e9d60f86fcb7fe16a6f624
Myeongjae Kim <dev@myeongjae.kim>
2026-06-29 01:35:25 +0900

    style: add web tui nerd font

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
       <button size-="large">click me too</button>
       <div box-="square">
         <p>content</p>
+        <span>
+          I use &#xf36f; and <span id="rust"></span>, btw
+        </span>
       </div>
     </div>
   );
