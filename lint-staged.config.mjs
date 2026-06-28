317e6bd71dd65819d8cb4728709f2e7387e8c168
Myeongjae Kim <dev@myeongjae.kim>
2026-06-29 01:52:52 +0900

    style: add _neodgm_pro

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,8 @@
-export default {
+const config = {
   '**/*.(mts|ts|tsx|js)': (filenames) => [
     `npx eslint --fix ${filenames.join(' ')}`,
     `npx prettier --write ${filenames.join(' ')}`,
   ],
 };
+
+export default config;
