7bfc279ad3d3781c03ea64c6e37a12ea0f7f7fe6
Myeongjae Kim <dev@myeongjae.kim>
2026-07-03 18:22:55 +0900

    chore: change cookie name

--- core/auth/config/CookieConfig.ts
+++ core/auth/config/CookieConfig.ts
@@ -1,6 +1,6 @@
 export class CookieConfig {
-  public readonly accessTokenCookieName = 'access_token';
-  public readonly refreshTokenCookieName = 'refresh_token';
+  public readonly accessTokenCookieName = '@hiking/access_token';
+  public readonly refreshTokenCookieName = '@hiking/refresh_token';
   public readonly accessTokenMaxAgeSeconds = 60 * 15;
   public readonly refreshTokenMaxAgeSeconds = 60 * 60 * 24 * 180;
 }
