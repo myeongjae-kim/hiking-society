d3d1368af61a5a34f1aebb3a7557c90f917927df
Myeongjae Kim <dev@myeongjae.kim>
2026-07-09 12:17:02 +0900

    chore: add biome

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,15 @@
 const config = {
-  '**/*.(mts|ts|tsx|js)': (filenames) => [`npx prettier --write ${filenames.join(' ')}`],
+  '**/*.(mts|ts|tsx|js)': (filenames) => {
+    const filteredFilenames = filenames.filter(
+      (filename) => !filename.endsWith('src/routeTree.gen.ts'),
+    );
+
+    if (filteredFilenames.length === 0) {
+      return [];
+    }
+
+    return [`pnpx biome check --fix ${filteredFilenames.join(' ')}`];
+  },
 };
 
 export default config;
